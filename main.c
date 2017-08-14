38136df4a3880cc4e50ab960f4b6eb24ea2d6f0f
onedesigners <chihtayip@gmail.com>
2017-08-14 12:31:07 +0800

    1

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  新手练习1
+//
+//  Created by one c on 14/8/2017.
+//  Copyright © 2017年 one c. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...这里是隐藏
+    printf("Hello, World!我爱你\n");
+    return 0;
+}
